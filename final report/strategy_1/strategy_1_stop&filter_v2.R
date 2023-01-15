85cb9199ef74818d6f2223977bfa69ac2b232cc2
curtis <curtisyan90@gmail.com>
2023-01-15 22:31:34 +0800

    Create stop&filter v2
    add long time std

--- final report/strategy_1/strategy_1_stop&filter_v2.R
+++ final report/strategy_1/strategy_1_stop&filter_v2.R
@@ -16,44 +16,54 @@ ma1 <- SMA(Cl(STK), ma1_len)
 ma2 <- SMA(Cl(STK), ma2_len)
 fee <- 0.0000229
 longStop <- (1 - 0.02) # stop when close < stop
-std_filter <- 3 # when std < filter
-d <- 4 # std previous days
 
 m <- 101 # start K
 c <- 0
 PL <- setNames(numeric(nrow(STK)), rownames(STK)) # position long
+# set std
+STD <- setNames(numeric(nrow(STK)), rownames(STK)) # std
+std_len <- 10
+d <- 4 # std previous days
+ld <- 30 # long std
+
 # OHLC
 op <- Op(STK)
 hi <- Hi(STK)
 lo <- Lo(STK)
 cl <- Cl(STK)
 
+n <- 50
+
 while (m < nrow(STK)) {
-     if ( # close crossover ma1 & ma1>ma2
-          ((cl[m - 1] <= ma1[m - 1] && cl[m] > ma1[m] && ma1[m] > ma2[m]) ||
-               # close crossover ma1 & ma2
-               (cl[m - 1] <= ma1[m - 1] && cl[m] > ma1[m] &&
-                    cl[m - 1] <= ma2[m - 1] && cl[m] > ma2[m])) &&
-               # ma2 growth
-               ma2[m] > ma2[m - 1] &&
-               # filter
-               sd(cl[(m - d):(m - 1)]) < std_filter
-     ) {
-          long <- as.numeric(Cl(STK)[m]) # buy price
-          c <- c + 1
-          while (
-               ## close < ma1
-               (Cl(STK)[m] < ma1[m] ||
-                    ## ma1 < ma2
-                    ma1[m] < ma2[m] ||
-                    ## close < stop
-                    cl[m] < long * longStop) == FALSE &&
-                    m < nrow(STK) - 1) {
-               m <- m + 1
-          }
-          PL[m] <- (as.numeric(Cl(STK)[m]) - long) * (1 - fee)
-     }
-     m <- m + 1
+  # STD[m] <- sd(cl[(m - d):(m - 1)])
+  if ( # close crossover ma1 & ma1>ma2
+    ((cl[m - 1] <= ma1[m - 1] && cl[m] > ma1[m] && ma1[m] > ma2[m]) ||
+      # close crossover ma1 & ma2
+      (cl[m - 1] <= ma1[m - 1] && cl[m] > ma1[m] &&
+        cl[m - 1] <= ma2[m - 1] && cl[m] > ma2[m])) &&
+      # ma2 growth
+      ma2[m] > ma2[m - 1] &&
+      # filter
+
+      # m > n &&
+      # sd(cl[(m - d):(m - 1)]) < sd(STD[(m - n):(m - 1)])
+      sd(cl[(m - d):(m - 1)]) < sd(cl[(m - ld):(m - 1)])
+  ) {
+    long <- as.numeric(Cl(STK)[m]) # buy price
+    c <- c + 1
+    while (
+      ## close < ma1
+      (Cl(STK)[m] < ma1[m] ||
+        ## ma1 < ma2
+        ma1[m] < ma2[m] ||
+        ## close < stop
+        cl[m] < long * longStop) == FALSE &&
+        m < nrow(STK) - 1) {
+      m <- m + 1
+    }
+    PL[m] <- (as.numeric(Cl(STK)[m]) - long) * (1 - fee)
+  }
+  m <- m + 1
 }
 
 ################################################################################
@@ -70,13 +80,13 @@ income <- sum(PL) # 總報酬
 ################################################################################
 yRange <- range(cumsum(PL), DD)
 plot(cumsum(PL),
-     type = "l", col = "red", lwd = 2, ylim = yRange, las = 1,
-     ylab = "PL", xlab = "Date", xaxt = "n"
+  type = "l", col = "red", lwd = 2, ylim = yRange, las = 1,
+  ylab = "PL", xlab = "Date", xaxt = "n"
 )
 par(new = TRUE)
 plot(DD,
-     type = "h", col = "blue", lwd = 2, ylim = yRange, axes = FALSE,
-     ylab = "", xlab = "", xaxt = "n"
+  type = "h", col = "blue", lwd = 2, ylim = yRange, axes = FALSE,
+  ylab = "", xlab = "", xaxt = "n"
 )
 
 points(which(DD == 0), cumsum(PL)[which(DD == 0)], pch = 10, col = "red")
@@ -84,9 +94,9 @@ points(which(DD == 0), cumsum(PL)[which(DD == 0)], pch = 10, col = "red")
 
 ## Add Legend
 legend("topleft",
-     legend = c("PL"),
-     text.col = c("red"), lwd = 3, col = c("red"), seg.len = 0.5,
-     cex = 0.75
+  legend = c("PL"),
+  text.col = c("red"), lwd = 3, col = c("red"), seg.len = 0.5,
+  cex = 0.75
 )
 
 # axis(1, 1:length(PL), as.Date("2012-01-01") + 1 * (1:length(PL)))
